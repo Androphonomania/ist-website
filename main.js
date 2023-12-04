3f5ea5a9b0018ac0317f67ab50ff20593cbd7310
Jack Kyogoto <bruhgyes@gmail.com>
2023-12-04 14:31:26 +1100

    added fallback page

--- main.js
+++ main.js
@@ -14,12 +14,20 @@ window.onload = function () {
             icon.setAttribute("href", "favicon.ico")
         }
     });
+
+    let aElements = document.getElementsByTagName("a");
+
+    for (let a of aElements) {
+        if (!a.hasAttribute("href") && !a.hasAttribute("class")) {
+            a.setAttribute("href", "fallback.html");
+        }
+    }
 }
 
-const shake = function() {
+const shake = function () {
     for (let i = 0; i < document.children.length; i++) {
         document.children[i].classList.add('animate__animated', 'animate__tada')
-        document.children[i].addEventListener('animationend', function() {
+        document.children[i].addEventListener('animationend', function () {
             document.children[i].classList.remove('animate__animated', 'animate__tada')
         })
     }
